d2e9afed43f39e7dd79553703f88d155bd05451a
Sergio Costa <sergio.costa@ufma.br>
2025-05-16 17:31:53 -0300

    update

--- docs/examples/gis/elevacao_mares_v2.py
+++ docs/examples/gis/elevacao_mares_v2.py
@@ -3,7 +3,7 @@ from dissmodel.core import Model, Environment
 
 from dissmodel.core.spatial import regular_grid, fill, dw_query
 
-from dissmodel.visualization import Map
+from dissmodel.visualization import Map, track_plot, Chart
 
 from dissmodel.visualization.streamlit import StreamlitMap, display_inputs
 
@@ -14,13 +14,19 @@ from matplotlib.colors import ListedColormap
 
 import geopandas as gpd
 
+@track_plot("media_geral", "red")
+@track_plot("media_mar", "blue")
 class Elevacao(Model):
 
     seaLevelRiseRate: float
+    media_geral : float
+    media_mar : float
 
     def setup (self, seaLevelRiseRate=0.011):
         self.seaLevelRiseRate = seaLevelRiseRate          # estrutura de vizinhança (ex: libpysal.weights)
-    
+        self.media_geral = 0
+        self.media_mar = 0
+
     def neighs(self, idx):
         """
         Retorna os índices dos vizinhos da célula fornecida.
@@ -30,22 +36,20 @@ class Elevacao(Model):
         else:
             return []
 
-    def rule_with_effects(self, idx):
+    def update_sea_level (self, idx):
+
         cell = self.env.gdf.loc[idx]
         viz_idxs = self.neighs(idx)
         
-        #affected = [idx]  # começa com a célula atual
-        affected = []  # começa com a célula atual
+        affected = [idx]  # começa com a célula atual
+        
 
         for v_idx in viz_idxs:
             if self.env.gdf.loc[v_idx, "Alt2"] < cell["Alt2"]:
                 affected.append(v_idx)
 
         n = len(affected)
-        if n > 0:
-            flow = self.seaLevelRiseRate / n
-        else:
-            flow = 0
+        flow = self.seaLevelRiseRate / n
         
         # retorna dicionário de atualizações
         return {i: flow for i in affected}
@@ -57,13 +61,18 @@ class Elevacao(Model):
         # Itera sobre os índices de interesse
         target_idxs = self.env.gdf[self.env.gdf["Usos"] == 3].index
         for idx in target_idxs:
-            updates = self.rule_with_effects(idx)
+            updates = self.update_sea_level(idx)
             for i, flow in updates.items():
-                delta[i] += flow
+                delta[i] += flow # pode receber agua de mais de uma celula
 
+        #print (delta)
         # Aplica todas as atualizações de uma vez
         self.env.gdf["Alt2"] += delta
 
+        self.media_geral = gdf["Alt2"].mean()
+        SEA_OR_FLOODED = [3, 6, 7, 9, 10]  # Correspondentes às constantes MAR, SOLO_DESCOBERTO_INUNDADO etc.
+        filtro_mar_ou_inundado = gdf["Usos"].isin(SEA_OR_FLOODED)
+        self.media_mar = gdf.loc[filtro_mar_ou_inundado, "Alt2"].mean()
 
 
 
@@ -93,6 +102,8 @@ plot_params={"column":'Alt2', "scheme":'quantiles', "k":3, "legend":True, "cmap"
 # Componente de visualização do mapa
 Map(plot_params=plot_params)
 
+Chart(select={"media_mar"})
+Chart(select={"media_geral"})
 
 ############################
 ### Execução da simulação
