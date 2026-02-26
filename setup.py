346381d1e73b86f20335a370ddf57e0f2b9777fe
Sergio Souza Costa <sergio.costa@ufma.br>
2026-02-26 14:15:51 -0300

    fix(packaging): bump to 0.1.5, consolidate deps in pyproject.toml and simplify setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="dissmodel",
-    version="0.1.2",
-    author="Sérgio Costa, Nerval Santos Junior",
-    description="dissmodel - Discrete Spatial Modeling",
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/LambdaGeo/dissmodel",
-    license="MIT",
-    packages=find_packages(),
-    install_requires=[
-        "numpy>=1.25.0",
-        # Outras dependências aqui
-    ],
-    python_requires=">=3.8",
-    classifiers=[
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    include_package_data=True,  # Inclui dados extra do MANIFEST.in
-)
+setup()
